42768fcf91b54eb9f072e16f802ffcbdbb75b56c
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-09-07 17:31:40 -0400

    a

--- GDD210_Quiz1-master/Assets/Scripts/LaunchCube.cs
+++ GDD210_Quiz1-master/Assets/Scripts/LaunchCube.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class LaunchCube : MonoBehaviour
 {
-	
+
+
+    public void OnMouseDown()
+    {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(0, 200, 0));
+    }
 }
