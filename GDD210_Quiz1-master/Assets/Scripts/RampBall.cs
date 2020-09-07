0f6c36057e2b048ffd5893a26eb3f29fa4bf2616
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-09-07 17:27:49 -0400

    uodate

---
+++ GDD210_Quiz1-master/Assets/Scripts/RampBall.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RampBall : MonoBehaviour
+{
+	public Text PositionText;
+
+    public void FixedUpdate()
+    {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(-Input.GetAxis("Horizontal")*20,0,0),ForceMode.Force) ;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        PositionText.text = "CENTER";
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag.Equals("Left"))
+        {
+            PositionText.text = "LEFT";
+        }
+        else {
+            if (collision.collider.tag.Equals("Right"))
+            {
+                PositionText.text = "RIGHT";
+            }
+        }
+    }
+
+}
