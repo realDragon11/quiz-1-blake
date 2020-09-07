2299bd30bb266e21b6ad0b18a4d5ab8ffa7afe7f
realDragon11 <43188351+realDragon11@users.noreply.github.com>
2020-09-07 17:36:26 -0400

    fin

--- GDD210_Quiz1-master/Assets/Scripts/MovePlayer.cs
+++ GDD210_Quiz1-master/Assets/Scripts/MovePlayer.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-	
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Z)) {
+        this.GetComponent<Rigidbody>().MovePosition(new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z-.1f)); }
+    }
 }
